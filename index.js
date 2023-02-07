e763811e52c24fcb93900bc20c6ed3f53cc9b110
MDNirobkhan1 <nirob937612@gmail.com>
2023-02-07 14:18:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+// --------Problem - 1------//
+// --------           -------//
+
+// function mindGame(numbers){
+//     const multiplication = numbers * 3;
+//     const addition = multiplication + 10;
+//     const division =addition / 2;
+//     const subtraction = division - 5 ;
+//     return subtraction;
+
+
+// }
+// const number = 20;
+// const result=mindGame(number);
+// console.log(result);
+
+
+
+// ------------ Problem - 2 ----------//
+// ------------            -----------///
+
+// function evenOdd(number){
+//     const total =number.length;
+//     if(total %2==0){
+//         console.log('even');
+//     }
+//     else{
+//         console.log('odd')
+//     }
+//     return total;
+    
+// }
+// const  string ='Mohiuddin islam Nirob';
+// const result = evenOdd(string);
+
+
+
+// ------------ Problem - 3 ----------//
+// ------------            -----------///
+
+
+// function isLGSeven(numbers){
+//     const subtraction = numbers - 7;
+//     if(subtraction < 7){
+//        return subtraction;
+//     }
+//     else {
+//         const total = numbers*2;
+//         return total;
+
+//     }
+// }
+// const num = 25;
+// const result = isLGSeven(num);
+// console.log(result);
+
+
+// ------------ Problem - 4 ----------//
+// ------------            -----------//
+
+// function findingBadData(data) {
+//     const badData = [];
+//     for (let i = 0; i < data.length; i++) {
+//       if (data[i] < 0) {
+//         badData.push(data[i]);
+//       }
+//     }
+//     return badData.length;
+//   }
+//   const data = [10, 20, -30, -40, 50, 60, -70, -80, -45];
+//   console.log(findingBadData(data));
+
+
+
+// ------------ Problem - 5 ----------//
+// ------------            -----------//
+
+// function gemsToDiamond(num1 , num2, num3){
+//     const sumNum1 = num1 * 21;
+//     const sumNum2 = num2 * 32;
+//     const sumNum3 = num3 * 43;
+//     const totalSum = sumNum1 + sumNum2 + sumNum3 ;
+//     if (totalSum > 2000){
+//         const subtraction = totalSum - 2000;
+//         return subtraction ;
+//     }
+//     else {
+//         return totalSum ;
+//     }
+// }
+
+// const firstFriendGem = 80;
+// const secondFriendGem = 30;
+// const thirdFriendGem = 20;
+// const gemResult = gemsToDiamond(firstFriendGem, secondFriendGem, thirdFriendGem);
+// console.log(gemResult);
+
